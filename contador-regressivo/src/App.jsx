3b651fa3629b892515234b51da6ce4fdc0f30f8f
Danilo Amorim <daniloamorim@MacBook-Pro-de-Danilo.local>
2024-08-11 10:23:02 -0300

    atualizando

--- contador-regressivo/src/App.jsx
+++ contador-regressivo/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import Title from './components/Title';
+import Counter from './components/Counter';
+//  pegar uma imagem  // import YOURBIRTHDAY from "./assets/yourbirthday.jpg"
+
 
 function App() {
   return (
-      <div className="App">
-          <h1>TESTE DOWN</h1>
+    <div className="App" style={{ backgroundImage: 'url(${YOURBIRTHDAY})' }}>
+      <div className="container">
+        <Title title="Contagem regressiva para 2025" />
+        <div className="countdown-container">
+          <Counter title="Dias" number={2} />
+          <Counter title="Horas" number={2} />
+          <Counter title="Minutos" number={2} />
+          <Counter title="Segundos" number={2} />
+        </div>
       </div>
+    </div>
   );
 }
 
